80c3f8eb99f6ebb8a6be85a64c80f153d97c2058
OyugoObonyo <nyikal1999@gmail.com>
2024-11-20 19:40:44 +0300

    * Push latest changes

--- src/registry/services/service.registry.ts
+++ src/registry/services/service.registry.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { UserService } from '@user/services/user.service';
 import { AuthService } from '@authentication/services/authentication.service';
 
 @Injectable()
-export class ServiceRegistry {
+export class ServiceRegistry implements OnModuleInit {
   // TODO: How to improve type safety?
   private services: { [key: string]: any } = {};
 
-  constructor(private readonly moduleRef: ModuleRef) {
+  constructor(private readonly moduleRef: ModuleRef) {}
+
+  onModuleInit(): void {
     this.services['AuthService'] = this.moduleRef.get(AuthService, {
       strict: false,
     });
     this.services['UserService'] = this.moduleRef.get(UserService, {
       strict: false,
     });
+    console.log(this.services);
   }
 
   getService(serviceName: string): any {
