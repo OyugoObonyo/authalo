883f794e85123f3c12bda2736ea2005fb50a7b8b
OyugoObonyo <nyikal1999@gmail.com>
2025-02-03 19:54:16 +0300

    * Push new to do

--- configs/jwt.config.ts
+++ configs/jwt.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('jwt', () => {
   return {
     secret: process.env.JWT_SECRET,
-    // TODO: Check best practices for aud and iss naming
+    // TODO: Check best practices for aud and iss naming. Audience could be $app-name?
     audience: 'Authalo-client',
     issuer: 'Authalo-server',
     accessTokenTtl: 3600,
